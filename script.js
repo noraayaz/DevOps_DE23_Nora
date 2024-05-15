900d880ee196982f91b1d7977da6e09f293e2bc6
noraAyaz <nur.89.ayaz@gmail.com>
2024-05-15 20:37:32 +0200

    index.html and script.js are updated

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const allMoviesTBody = document.querySelector("#allItems tbody")
+// Functions that will run when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    updateCurrentDate();
+    loadTableData();
+});
 
-const showTable = function(moviesArray){
-    allMoviesTBody.innerHTML = ""
-    for(let i = 0; i < moviesArray.length;i++) { 
-        let trText = `<tr><th scope="row">${moviesArray[i].Givenname} ${moviesArray[i].Surname}</th><td>${moviesArray[i].Country}</td><td>${moviesArray[i].Birthday.substring(0,4)}</td></tr>`
-        allMoviesTBody.innerHTML += trText
+// Function to update the date and time information in the header
+function updateCurrentDate() {
+    const currentDateElement = document.getElementById('currentDate');
+    const now = new Date();
+    const dateString = now.toISOString().slice(0, 10); // Date in YYYY-MM-DD format
+    const timeString = now.toTimeString().slice(0, 5); // Time in HH:MM format
+    currentDateElement.textContent = `${dateString} ${timeString} The current cool customers`;
+}
+
+// Function to load data and update the table
+function loadTableData() {
+    const allMoviesTBody = document.querySelector("#allItems tbody");
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((json) => {
+            showTable(json);
+        });
+
+    function showTable(moviesArray) {
+        allMoviesTBody.innerHTML = "";
+        for (let i = 0; i < moviesArray.length; i++) {
+            let trText = `<tr><th scope="row">${moviesArray[i].Givenname} ${moviesArray[i].Surname}</th><td>${moviesArray[i].Country}</td><td>${moviesArray[i].Birthday.substring(0,4)}</td></tr>`;
+            allMoviesTBody.innerHTML += trText;
+        }
     }
-
-} 
-
-fetch('./data.json')
-    .then((response) => response.json())
-    .then((json) => {
-        showTable(json)
-    });
+}
